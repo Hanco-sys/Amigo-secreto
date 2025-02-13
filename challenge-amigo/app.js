68f4fa08d42ec8df77c6a096d8b1beea3be69576
Harold <haroldmoren2017@gmail.com>
2025-02-13 11:20:07 -0500

    Añadir amigo secreto

--- challenge-amigo/app.js
+++ challenge-amigo/app.js
@@ -1,1 +1,15 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigoSecreto = [];
+
+function amigoSecretoFunction() {
+    let listaAmigoSecreto = document.getElementById('amigo').value;
+    if (listaAmigoSecreto === '') {
+        alert('Por favor, ingrese un nombre');
+        return;
+    }
+    amigoSecreto.push(listaAmigoSecreto);
+    console.log(amigoSecreto);
+    return amigoSecreto;
+
+};
